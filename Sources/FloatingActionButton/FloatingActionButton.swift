d75d43becb862020eebfacd73a1fc027ac002ac3
Doug Diego <doug@diego.org>
2021-02-19 07:08:47 -0800

    Updating UI of FAB

--- Sources/FloatingActionButton/FloatingActionButton.swift
+++ Sources/FloatingActionButton/FloatingActionButton.swift
@@ -6,7 +6,8 @@
 //
 import SwiftUI
 
-public struct FloatingItem {
+public struct FloatingItem: Identifiable {
+    public let id = UUID()
     let title: String
     let image: Image
     let action: () -> Void
@@ -18,16 +19,117 @@ public struct FloatingItem {
     }
 }
 
-public struct FloatingActionButton<Parent: View>: View {
-    @State private var showingPrefillSheet = false
+struct FloatingActionButton<Parent: View>: View {
+    @State private var showingFloatingItems = false
     var parent: Parent
-    let title: String
     let image: Image
+    let activeImage: Image?
+    let color: Color?
     let items: [FloatingItem]
     
-    init(parent: Parent, title: String, image: Image, items: [FloatingItem]) {
+    init(parent: Parent,
+         image: Image,
+         activeImage: Image,
+         color:  Color,
+         items: [FloatingItem]
+    ) {
+        self.parent = parent
+        self.image = image
+        self.activeImage = activeImage
+        self.color = color
+        self.items = items
+    }
+    
+    public var body: some View {
+        ZStack {
+            parent
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            self.showingFloatingItems.toggle()
+                        }
+                    }) {
+                        if self.showingFloatingItems {
+                            activeImage
+                        } else {
+                            image
+                        }
+                    }
+                    .padding(12)
+                    .background(color)
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+                
+                
+            }
+            .padding(.bottom, 60)
+            
+            if showingFloatingItems {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        VStack {
+                            ForEach(items, id: \.id) { item in
+                                Label {
+                                    Text(item.title).font(.title2)
+                                } icon: {
+                                    item.image.font(.title2)
+                                }
+                                .padding(.leading)
+                                .padding(.vertical, 8)
+                                //.background(Color.red)
+                                .onTapGesture {
+                                    item.action()
+                                    withAnimation {
+                                        self.showingFloatingItems.toggle()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.trailing)
+                .padding(.bottom, 140)
+                .transition(.move(edge: .trailing))
+            }
+        }
+    }
+}
+
+public struct FloatingActionSheetItem: Identifiable {
+    public let id = UUID()
+    let title: String
+    let action: () -> Void
+    
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+}
+
+struct FloatingActionSheetButton<Parent: View>: View {
+    @State private var showingFloatingItems = false
+    var parent: Parent
+    let title: String
+    let cancelTitle: String
+    let image: Image
+    let items: [FloatingActionSheetItem]
+    
+    init(parent: Parent,
+         title: String,
+         cancelTitle: String,
+         image: Image,
+         items: [FloatingActionSheetItem]) {
         self.parent = parent
         self.title = title
+        self.cancelTitle = cancelTitle
         self.image = image
         self.items = items
     }
@@ -40,14 +142,14 @@ public struct FloatingActionButton<Parent: View>: View {
                 HStack {
                     Spacer()
                     Button(action: {
-                        self.showingPrefillSheet = true
+                        self.showingFloatingItems = true
                     }) {
                         image
                     }
-                    .actionSheet(isPresented: self.$showingPrefillSheet) {
+                    .actionSheet(isPresented: self.$showingFloatingItems) {
                         ActionSheet(title: Text(title),
                                     message: nil,
-                                    buttons: getButtons()
+                                    buttons: getActionSheetButtons()
                         )
                     }
                     .padding(12)
@@ -57,24 +159,31 @@ public struct FloatingActionButton<Parent: View>: View {
                     .clipShape(Circle())
                     .padding(.trailing)
                 }
-                
             }.padding(.bottom, 60)
         }
     }
     
-    func getButtons() -> [ActionSheet.Button]  {
+    func getActionSheetButtons() -> [ActionSheet.Button]  {
         var buttons = [ActionSheet.Button]()
         for item in items {
-            print("item: \(item)")
             buttons.append(ActionSheet.Button.default(Text(item.title), action: item.action))
         }
-        buttons.append(ActionSheet.Button.cancel(Text("Cancel")))
+        buttons.append(ActionSheet.Button.cancel(Text(cancelTitle)))
         return buttons
     }
+    
 }
 
 extension View {
-    public func fab(title: String, image: Image, items: [FloatingItem]) -> some View {
-        FloatingActionButton(parent: self, title: title, image: image, items: items)
+    public func fab(image: Image, items: [FloatingItem]) -> some View {
+        FloatingActionButton(parent: self, image: image, activeImage: image, color:  Color.black.opacity(0.75), items: items)
+    }
+    
+    public func fab(image: Image, activeImage: Image, color: Color, items: [FloatingItem]) -> some View {
+        FloatingActionButton(parent: self, image: image, activeImage: activeImage, color: color, items: items)
+    }
+    
+    public func fasb(title: String, cancelTitle: String, image: Image, items: [FloatingActionSheetItem]) -> some View {
+        FloatingActionSheetButton(parent: self, title: title, cancelTitle: cancelTitle, image: image, items: items)
     }
 }
